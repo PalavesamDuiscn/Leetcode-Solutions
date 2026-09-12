2f83e94d5505b8296bd5427738b5327bb9318e51
Palavesam Duiscn <palavesamduiscn@gmail.com>
2026-09-12 10:40:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.27%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.py
@@ -0,0 +1,11 @@
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+
+        s=set()
+
+        for path in paths:
+            s.add(path[0])
+
+        for path in paths:
+            if path[1] not in s:
+                return path[1]
